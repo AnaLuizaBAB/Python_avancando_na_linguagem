b1c12db6942e961f5e4dae3ff22ea2ce826e080e
AnaLuizaBAB <aninhat17@gmail.com>
2024-02-11 10:28:29 -0300

    Módulo 2

--- forca.py
+++ forca.py
@@ -4,6 +4,19 @@ def jogar():
     print ("Bem vindo ao jogo da forca.")
     print("\n")
 
+    
+    palavra_secreta = 'banana'
+    enforcado = False
+    acertou = False
+
+    while((enforcado == False) and (acertou == False)):
+        index = 0                                                           # se o index for declarado antes do while retornará a posição errada.
+        chute = (input("Digite uma letra:")).lower().strip()
+        for letra in palavra_secreta:
+            if (letra == chute):
+                print( f"A letra {chute} foi encontrada na posição {index}")
+            index = index + 1
+        print("Tente novamente.")
 
     print("\nFim de Jogo.")
 
