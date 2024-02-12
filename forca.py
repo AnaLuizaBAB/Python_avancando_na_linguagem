02466783fb012efbc08693867a1dfe74bcaade08
AnaLuizaBAB <aninhat17@gmail.com>
2024-02-12 13:58:09 -0300

    Guardando os chutes do usuário em uma lista

--- forca.py
+++ forca.py
@@ -9,14 +9,25 @@ def jogar():
     enforcado = False
     acertou = False
 
+    lista = []
+
+    for letra in palavra_secreta:                                           # para colocar a quantidade de "_" de acordo com o tamanho da palavra secreta!
+        lista.append('_')
+    
+    print (lista)
+    print('\n')
+
     while((enforcado == False) and (acertou == False)):
         index = 0                                                           # se o index for declarado antes do while retornará a posição errada.
-        chute = (input("Digite uma letra:")).lower().strip()
+        chute = (input("Digite uma letra:\n")).lower().strip()
         for letra in palavra_secreta:
             if (letra == chute):
-                print( f"A letra {chute} foi encontrada na posição {index}")
+                lista.pop(index)                                            # Retira o '_' na posição indicada e na linha abaixo é substituida pela letra correspondente.
+                lista.insert(index, chute)
             index = index + 1
-        print("Tente novamente.")
+        print(lista)
+        
+        print("\nTente novamente.")
 
     print("\nFim de Jogo.")
 
