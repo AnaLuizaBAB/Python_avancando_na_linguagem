15cb5d898f8c0e8efcc2b5240dc8be5204cb92c7
AnaLuizaBAB <aninhat17@gmail.com>
2024-02-16 20:28:23 -0300

    Módulo 5, List Comprehension

--- forca.py
+++ forca.py
@@ -8,9 +8,7 @@ def jogar():
     palavra_secreta = 'banana'.lower()
     enforcado = False
     acertou = False
-    numero_da_tentativa = 1                                                 # se eu colocar esse index dentro do wile da errado!! PQ??
-
-    # decidir o nível de dificuldade
+    numero_da_tentativa = 1                                                
 
     print ("Informe o nível de dificuldade desejada")
     print("1 (Fácil)     2 (Médio)     3 (Difícil)")
@@ -25,10 +23,10 @@ def jogar():
 
     # Colocando um '_' para cada letra da palavra secreta
         
-    lista = []
 
-    for letra in palavra_secreta:                                           # para colocar a quantidade de "_" de acordo com o tamanho da palavra secreta!
-        lista.append('_')
+
+    lista = ["_" for letra in palavra_secreta]                                          # para colocar a quantidade de "_" de acordo com o tamanho da palavra secreta!
+
     
     print ('\n')
     print (lista)
@@ -46,15 +44,17 @@ def jogar():
             index = index + 1
 
         if(lista.count("_")) == 0:
+            print(f"\nTentativa {numero_da_tentativa}/{tentativas}")
             print(lista)
             print("\nParabéns! Você acertou a palavra.")
             break
         elif(numero_da_tentativa == tentativas) and ((lista.count("_")) != 0):
+            print(f"\nTentativa {numero_da_tentativa}/{tentativas}")
             print(lista)
             print("Você foi enforcado!!!")
             break
         else:
-            print(f"Tentativa {numero_da_tentativa}")
+            print(f"\nTentativa {numero_da_tentativa}/{tentativas}")
             print(lista)
         numero_da_tentativa = numero_da_tentativa + 1
 
